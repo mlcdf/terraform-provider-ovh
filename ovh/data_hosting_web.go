775d2ded8733c294652e4ba5906d1a8a9bc70afe
Maxime Le Conte des Floris <git@mlcdf.fr>
2022-12-28 13:56:15 +0100

    wip

---
+++ ovh/data_hosting_web.go
@@ -0,0 +1,278 @@
+package ovh
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceHostingWeb() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceHostingWebRead,
+		Schema: map[string]*schema.Schema{
+			"service_name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			// Computed
+			"available_boost_offer": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Available offers for boost option",
+			},
+			"boost_offer": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Current boost offer",
+			},
+			"cluster": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Cluster name",
+			},
+			"cluster_ip": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "ip of the cluster",
+			},
+			"cluster_ipv6": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "ipv6 of the cluster",
+			},
+			"countries_ip": {
+				Type:        schema.TypeSet,
+				Computed:    true,
+				Description: "Available clusterIp by countries",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"country": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"ip": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"ipv6": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"datacenter": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Datacenter where this service is located",
+			},
+			"display_name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Name displayed in customer panel for your web hosting",
+			},
+			"filer": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Filer name",
+			},
+			"has_cdn": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Has a CDN service linked on the hosting",
+			},
+			"has_hosted_ssl": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Has a HostedSSL service linked on the hosting",
+			},
+			"home": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Path of the home directory",
+			},
+			"hosting_ip": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "IP of the hosting",
+			},
+			"hosting_ipv6": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "IPv6 of the hosting",
+			},
+			"last_ovh_config_scan": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Date of last ovhConfig scan",
+			},
+			"offer": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Hosting's offer",
+			},
+			"operation_system": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Hosting's operating system",
+			},
+			"php_versions": {
+				Type:        schema.TypeSet,
+				Computed:    true,
+				Description: "List of availables PHP versions for this hosting",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"support": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"version": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"primary_login": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Hosting's main login",
+			},
+			"quota_size": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Space used (in MB)",
+			},
+			"quota_used": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Space allowed (in MB)",
+			},
+			"recommended_offer": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "If your offer is old, return a recommended offer to migrate on",
+			},
+			"resource_type": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Hosting resource type",
+			},
+			"service_management_access": {
+				Type:        schema.TypeMap,
+				Computed:    true,
+				Description: "Private database state",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"ssh": {
+							Type:     schema.TypeMap,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"port": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"url": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+						"ftp": {
+							Type:     schema.TypeMap,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"port": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"url": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+						"http": {
+							Type:     schema.TypeMap,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"port": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"url": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			"state": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Private database state",
+			},
+			"token": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Private database state",
+			},
+			"traffic_quota_size": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Space allowed (in MB) on your private database",
+			},
+			"traffic_quota_used": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Space allowed (in MB) on your private database",
+			},
+			"updates": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Private database state",
+			},
+		},
+	}
+}
+
+func dataSourceHostingWebRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	serviceName := d.Get("service_name").(string)
+
+	ds := &HostingWeb{}
+	err := config.OVHClient.Get(
+		fmt.Sprintf(
+			"/hosting/web/%s",
+			url.PathEscape(serviceName),
+		),
+		&ds,
+	)
+
+	if err != nil {
+		return fmt.Errorf(
+			"Error calling hosting/web/%s:\n\t %q",
+			serviceName,
+			err,
+		)
+	}
+
+	for k, v := range ds.ToMap() {
+		if k != "service_name" {
+			d.Set(k, v)
+		}
+	}
+	d.SetId(ds.ServiceName)
+
+	return nil
+}
---
+++ ovh/types_hosting_web.go
@@ -0,0 +1,406 @@
+package ovh
+
+import (
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/ovh/terraform-provider-ovh/ovh/helpers"
+)
+
+type HostingWeb struct {
+	ServiceName             string                            `json:"serviceName"`
+	AvailableBoostOffer     []HostingWebAvailableBoostOffer   `json:"availableBoostOffer"`
+	BoostOffer              *string                           `json:"boostOffer"`
+	CountriesIP             []HostingWebCountryIp             `json:"countriesIp"`
+	Cluster                 string                            `json:"cluster"`
+	ClusterIP               string                            `json:"clusterIp"`
+	ClusterIPv6             string                            `json:"clusterIpv6"`
+	DisplayName             *string                           `json:"displayName"`
+	Datacenter              string                            `json:"datacenter"`
+	Filer                   string                            `json:"filer"`
+	HasCDN                  bool                              `json:"hasCdn"`
+	HasHostedSSL            bool                              `json:"hasHostedSsl"`
+	Home                    string                            `json:"home"`
+	HostingIP               string                            `json:"hostingIp"`
+	HostingIPv6             string                            `json:"hostingIpv6"`
+	LastOVHConfigScan       *time.Time                        `json:"lastOvhConfigScan"`
+	Offer                   string                            `json:"offer"`
+	OperationSystem         string                            `json:"operationSystem"`
+	PHPVersions             []HostingWebVersion               `json:"phpVersions"`
+	PrimaryLogin            string                            `json:"primaryLogin"`
+	QuotaSize               *UnitAndValue                     `json:"quotasize"`
+	QuotaUsed               *UnitAndValue                     `json:"quotaUsed"`
+	RecommendedOffer        *string                           `json:"recommendedOffer"`
+	ResourceType            string                            `json:"resourceType"`
+	ServiceManagementAccess HostingWebServiceManagementAccess `json:"serviceManagementAccess"`
+	State                   string                            `json:"state"`
+	Token                   string                            `json:"token"`
+	TrafficQuotaSize        *UnitAndValue                     `json:"trafficQuotaSize"`
+	TrafficQuotaUsed        *UnitAndValue                     `json:"trafficQuotaUsed"`
+	Updates                 []string                          `json:"updates"`
+}
+
+type HostingWebAvailableBoostOffer struct {
+	Price OrderCartGenericProductPricePrice `json:"price"`
+	Offer string                            `json:"offer"`
+}
+
+func (v HostingWebAvailableBoostOffer) ToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["price"] = v.Price.ToMap()
+	obj["offer"] = v.Offer
+
+	return obj
+}
+
+type HostingWebCountryIp struct {
+	Country string `json:"country"`
+	IP      string `json:"ip"`
+	IPv6    string `json:"ipv6"`
+}
+
+func (v HostingWebCountryIp) ToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["country"] = v.Country
+	obj["ip"] = v.IP
+	obj["ipv6"] = v.IPv6
+
+	return obj
+}
+
+type HostingWebServiceManagementAccess struct {
+	SSH  HostingWebAddress `json:"ssh"`
+	FTP  HostingWebAddress `json:"ftp"`
+	HTTP HostingWebAddress `json:"http"`
+}
+
+func (v HostingWebServiceManagementAccess) ToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["ssh"] = v.SSH.ToMap()
+	obj["ftp"] = v.FTP.ToMap()
+	obj["http"] = v.HTTP.ToMap()
+
+	return obj
+}
+
+type HostingWebVersion struct {
+	Support string `json:"support"`
+	Version string `json:"version"`
+}
+
+func (v HostingWebVersion) ToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["support"] = v.Support
+	obj["version"] = v.Version
+
+	return obj
+}
+
+type HostingWebAddress struct {
+	Port int    `json:"port"`
+	URL  string `json:"url"`
+}
+
+func (v HostingWebAddress) ToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["port"] = v.Port
+	obj["url"] = v.URL
+
+	return obj
+}
+
+func (v HostingWeb) ToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["service_name"] = v.ServiceName
+
+	availableBoostOffer := []map[string]interface{}{}
+	for _, v := range v.AvailableBoostOffer {
+		availableBoostOffer = append(availableBoostOffer, v.ToMap())
+	}
+
+	obj["available_boost_offer"] = availableBoostOffer
+
+	obj["boost_offer"] = v.BoostOffer
+	obj["cluster"] = v.Cluster
+	obj["cluster_ip"] = v.ClusterIP
+	obj["cluster_ipv6"] = v.ClusterIPv6
+
+	countriesIP := []map[string]interface{}{}
+	for _, v := range v.CountriesIP {
+		countriesIP = append(countriesIP, v.ToMap())
+	}
+
+	obj["countries_ip"] = countriesIP
+
+	obj["datacenter"] = v.Datacenter
+	obj["display_name"] = v.DisplayName
+	obj["filer"] = v.Filer
+	obj["has_cdn"] = v.HasCDN
+	obj["has_hosted_ssl"] = v.HasHostedSSL
+	obj["home"] = v.Home
+	obj["hosting_ip"] = v.HostingIP
+	obj["hosting_ipv6"] = v.HostingIPv6
+	obj["last_ovh_config_scan"] = v.LastOVHConfigScan
+	obj["offer"] = v.Offer
+	obj["operation_system"] = v.OperationSystem
+
+	phpVersions := []map[string]interface{}{}
+	for _, v := range v.PHPVersions {
+		phpVersions = append(phpVersions, v.ToMap())
+	}
+
+	obj["php_versions"] = phpVersions
+
+	obj["primary_login"] = v.PrimaryLogin
+	obj["quota_size"] = v.QuotaSize.Value
+	obj["quota_used"] = v.QuotaUsed.Value
+	obj["recommended_offer"] = v.RecommendedOffer
+	obj["resource_type"] = v.ResourceType
+	obj["service_management_access_ftp_url"] = v.ServiceManagementAccess.FTP.URL
+	obj["service_management_access_ftp_port"] = v.ServiceManagementAccess.FTP.Port
+	obj["service_management_access_http_url"] = v.ServiceManagementAccess.HTTP.URL
+	obj["service_management_access_http_port"] = v.ServiceManagementAccess.HTTP.Port
+	obj["service_management_access_ssh_url"] = v.ServiceManagementAccess.SSH.URL
+	obj["service_management_access_ssh_port"] = v.ServiceManagementAccess.SSH.Port
+	obj["state"] = v.State
+	obj["token"] = v.Token
+	obj["traffic_quota_size"] = v.TrafficQuotaSize.Value
+	obj["traffic_quota_used"] = v.TrafficQuotaUsed.Value
+	obj["updates"] = v.Updates
+
+	return obj
+}
+
+type HostingWebOpts struct {
+	DisplayName *string `json:"displayName"`
+}
+
+func (opts *HostingWebOpts) FromResource(d *schema.ResourceData) *HostingWebOpts {
+	opts.DisplayName = helpers.GetNilStringPointerFromData(d, "display_name")
+
+	return opts
+}
+
+type HostingWebConfirmTerminationOpts struct {
+	Token string `json:"token"`
+}
+
+type DataSourceHostingWebDatabase struct {
+	BackupTime   string                               `json:"backupTime"`
+	QuotaUsed    *UnitAndValue                        `json:"quotaUsed"`
+	CreationDate string                               `json:"creationDate"`
+	Users        []*DataSourceHostingWebDatabaseUsers `json:"users"`
+}
+
+func (v DataSourceHostingWebDatabase) ToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["backup_time"] = v.BackupTime
+	obj["quota_used"] = v.QuotaUsed.Value
+	obj["creation_date"] = v.CreationDate
+
+	var users []map[string]interface{}
+	for _, r := range v.Users {
+		users = append(users, r.ToMap())
+	}
+	obj["users"] = users
+	return obj
+}
+
+type DataSourceHostingWebDatabaseUsers struct {
+	UserName  string `json:"userName"`
+	GrantType string `json:"grantType"`
+}
+
+func (v DataSourceHostingWebDatabaseUsers) ToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["user_name"] = v.UserName
+	obj["grant_type"] = v.GrantType
+	return obj
+}
+
+type HostingWebDatabase struct {
+	DoneDate     string `json:"doneDate"`
+	LastUpdate   string `json:"lastUpdate"`
+	UserName     string `json:"userName"`
+	DumpId       string `json:"dumpId"`
+	DatabaseName string `json:"databaseName"`
+	TaskId       int    `json:"id"`
+	StartDate    string `json:"startDate"`
+	Status       string `json:"status"`
+}
+
+func (v HostingWebDatabase) ToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["database_name"] = v.DatabaseName
+	return obj
+}
+
+type HostingWebDatabaseCreateOpts struct {
+	DatabaseName string `json:"databaseName"`
+}
+
+func (opts *HostingWebDatabaseCreateOpts) FromResource(d *schema.ResourceData) *HostingWebDatabaseCreateOpts {
+	opts.DatabaseName = d.Get("database_name").(string)
+
+	return opts
+}
+
+type DataSourceHostingWebUser struct {
+	CreationDate string                               `json:"creationDate"`
+	Databases    []*DataSourceHostingWebUserDatabases `json:"databases"`
+}
+
+func (v DataSourceHostingWebUser) ToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["creation_date"] = v.CreationDate
+
+	var databases []map[string]interface{}
+	for _, r := range v.Databases {
+		databases = append(databases, r.ToMap())
+	}
+	obj["databases"] = databases
+	return obj
+}
+
+type DataSourceHostingWebUserDatabases struct {
+	DatabaseName string `json:"databaseName"`
+	GrantType    string `json:"grantType"`
+}
+
+func (v DataSourceHostingWebUserDatabases) ToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["database_name"] = v.DatabaseName
+	obj["grant_type"] = v.GrantType
+	return obj
+}
+
+type HostingWebUser struct {
+	LastUpdate   string `json:"lastUpdate"`
+	DoneDate     string `json:"doneDate"`
+	Status       string `json:"status"`
+	StartDate    string `json:"startDate"`
+	DatabaseName string `json:"databaseName"`
+	UserName     string `json:"userName"`
+	TaskId       int    `json:"id"`
+}
+
+func (v HostingWebUser) ToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["user_name"] = v.UserName
+	return obj
+}
+
+type HostingWebUserCreateOpts struct {
+	Password string `json:"password"`
+	UserName string `json:"userName"`
+}
+
+func (opts *HostingWebUserCreateOpts) FromResource(d *schema.ResourceData) *HostingWebUserCreateOpts {
+	opts.Password = d.Get("password").(string)
+	opts.UserName = d.Get("user_name").(string)
+
+	return opts
+}
+
+type HostingWebUserGrant struct {
+	LastUpdate   string `json:"lastUpdate"`
+	DoneDate     string `json:"doneDate"`
+	Status       string `json:"status"`
+	StartDate    string `json:"startDate"`
+	DatabaseName string `json:"databaseName"`
+	UserName     string `json:"userName"`
+	TaskId       int    `json:"id"`
+}
+
+type HostingWebUserGrantCreateOpts struct {
+	DatabaseName string `json:"databaseName"`
+	Grant        string `json:"grant"`
+}
+
+func (opts *HostingWebUserGrantCreateOpts) FromResource(d *schema.ResourceData) *HostingWebUserGrantCreateOpts {
+	opts.DatabaseName = d.Get("database_name").(string)
+	opts.Grant = d.Get("grant").(string)
+
+	return opts
+}
+
+func (v HostingWebUserGrantCreateOpts) ToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["grant"] = v.Grant
+	obj["database_name"] = v.DatabaseName
+	return obj
+}
+
+type DataSourceHostingWebUserGrant struct {
+	CreationDate string `json:"creationDate"`
+	Grant        string `json:"grant"`
+}
+
+func (v DataSourceHostingWebUserGrant) ToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["creation_date"] = v.CreationDate
+	obj["grant"] = v.Grant
+
+	return obj
+}
+
+type HostingWebWhitelist struct {
+	CreationDate string `json:"creationDate"`
+	LastUpdate   string `json:"lastUpdate"`
+	Name         string `json:"name"`
+	Service      bool   `json:"service"`
+	Sftp         bool   `json:"sftp"`
+	Status       string `json:"status"`
+	TaskId       int    `json:"id"`
+}
+
+func (v HostingWebWhitelist) ToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["name"] = v.Name
+	obj["service"] = v.Service
+	obj["sftp"] = v.Sftp
+
+	return obj
+}
+
+func (v HostingWebWhitelist) DataSourceToMap() map[string]interface{} {
+	obj := make(map[string]interface{})
+	obj["name"] = v.Name
+	obj["service"] = v.Service
+	obj["sftp"] = v.Sftp
+	obj["creation_date"] = v.CreationDate
+	obj["last_update"] = v.LastUpdate
+	obj["status"] = v.Status
+
+	return obj
+}
+
+type HostingWebWhitelistCreateOpts struct {
+	Ip      string `json:"ip"`
+	Name    string `json:"name"`
+	Service bool   `json:"service"`
+	Sftp    bool   `json:"sftp"`
+}
+
+func (opts *HostingWebWhitelistCreateOpts) FromResource(d *schema.ResourceData) *HostingWebWhitelistCreateOpts {
+	opts.Ip = d.Get("ip").(string)
+	opts.Name = d.Get("name").(string)
+	opts.Service = d.Get("service").(bool)
+	opts.Sftp = d.Get("sftp").(bool)
+
+	return opts
+}
+
+type HostingWebWhitelistUpdateOpts struct {
+	Name    string `json:"name"`
+	Service bool   `json:"service"`
+	Sftp    bool   `json:"sftp"`
+}
+
+func (opts *HostingWebWhitelistUpdateOpts) FromResource(d *schema.ResourceData) *HostingWebWhitelistUpdateOpts {
+	opts.Name = d.Get("name").(string)
+	opts.Service = d.Get("service").(bool)
+	opts.Sftp = d.Get("sftp").(bool)
+
+	return opts
+}
